8020d7f53a8efa30ebfb7ad8db946caebe838b1d
Dmitry Yudakov <yudakov.d@mail.ru>
2022-08-19 00:00:21 +0300

    move to sockets

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React, {useContext, useState} from 'react';
+import {Link} from "react-router-dom";
+import {AuthContext} from "../../context";
+import Logo from "./Logo/Logo";
+import SearchNav from "./SearchNav/SearchNav";
+import cl from "./Navbar.module.css";
+import LinksNav from "./LinksNav/LinksNav";
+import AsideNav from "../AsideNav/AsideNav";
+import MyModal from "../UI/MyModal/MyModal";
+
+const Navbar = () => {
+
+	const {isAuth, setIsAuth} = useContext(AuthContext);
+	const [showMenu, setShowMenu] = useState(false);
+
+	const logout = () => {
+		setIsAuth(false);
+		localStorage.removeItem('auth');
+	}
+
+	return (
+		<nav className={cl.navbar}>
+			<div className={cl.navbar__content}>
+				<Link to="/"><Logo/></Link>
+				<SearchNav/>
+
+				<div className={cl.show_menu}
+						 onClick={() => setShowMenu(!showMenu)}>
+					Меню
+				</div>
+
+				<div className={cl.navbar__links}>
+					<a className={cl.navbar_link} onClick={logout}>Выйти</a>
+				</div>
+
+				<MyModal visible={showMenu} setVisible={setShowMenu}
+								 children={<AsideNav isAbsolute={true}/>}
+				/>
+
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
